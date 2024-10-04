7d00fd7fcc95517f5650ab40d2b8cf91a8b0f480
frodr003 <jbond007@doc.gold.ac.uk>
2024-10-04 12:08:18 +0100

    update

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -1,6 +1,6 @@
 # Insert data into the tables
 
-USE bettys_books;
+USE myBookshop;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal farm', 12.99) ;
 
